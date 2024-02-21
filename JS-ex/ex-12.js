11a10adaf27a6d5b1f7642add50ed2e656ad0761
Lucas Grander <lucasgrander@gmail.com>
2024-02-21 01:15:47 -0300

    ORIGINAL UPDATE - 21/02 - REORGANIZADO

--- JS-ex/ex-12.js
+++
@@ -1,9 +0,0 @@
-const namelist = ["Lucas", "Ricardo", "José", "Ana", "Bia"];
-
-const search = "Ana";
-
-if(namelist.includes(search)){
-    console.log(`O nome encontrado na lista foi: ${search}`);
-} else{
-    console.log(`O nome ${search} não foi encontrado na lista`);
-}
