eb2d0e7734e18ecc4d43532f184d63e77cb93506
LucasGrander <157768888+LucasGrander@users.noreply.github.com>
2024-03-13 11:28:27 -0300

    save errado dos arquivos

--- PROGRAMADOR-WEB/React(jsx)/first-react-project/src/App.tsx
+++ PROGRAMADOR-WEB/React(jsx)/first-react-project/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//para que o componente que eu criei possa aparecer em minha página, agora tenho que fazer uma importação do mesmo, sempre usando IMPORT e FROM 
+import MyInput from './components/MyInput';
 
+
+
+//aqui criamos um componenete App para armazenar todos os componetes da minha página - sempre usar return(código...)
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    //aqui colocamos um <> e fechamos o code com </>, dentro disso será implementado os componentes criados... com o nome do componete dado na hora de sua criação, como aqui: MyInput ----> fazendo isso, agora podermos editar esses botões conforme os props que criamos, colocando o nome do prop e um valor existente para ele
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <MyInput
+      type="text"
+      placeholder='Username'/>
 
+      <MyInput
+      type="password"
+      placeholder='Password'/>
+    </>
+  );
+}
+// exportação padrão para qualquer componete criado
 export default App
