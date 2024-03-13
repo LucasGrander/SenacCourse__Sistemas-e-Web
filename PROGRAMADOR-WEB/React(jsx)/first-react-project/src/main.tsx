eb1934bdc653babb213b9176ce042dcbdc4dfadd
Lucas Grander <lucasgrander@gmail.com>
2024-03-13 19:34:06 -0300

    Revert "save errado dos arquivos"
    This reverts commit eb2d0e7734e18ecc4d43532f184d63e77cb93506.

--- PROGRAMADOR-WEB/React(jsx)/first-react-project/src/main.tsx
+++ PROGRAMADOR-WEB/React(jsx)/first-react-project/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
 
-
-//aqui iremos renderizar todos os componentes criados, que por sua vez foi importado acima, e mencionado da seguinte forma: <App />
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
