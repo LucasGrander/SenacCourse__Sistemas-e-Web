f78d9d11f8ac2b98d5a53d5a7b2fecd383617f4c
LucasGrander <157768888+LucasGrander@users.noreply.github.com>
2024-04-24 11:24:47 -0300

    API - Rest

---
+++ PROGRAMADOR-WEB/API/API-Rest/controllers/userControllers.js
@@ -0,0 +1,41 @@
+import { db } from '../database/db.js'
+
+export const getUsers = (_, res) => {
+    const sql = "select * from usuario"
+
+    db.query (sql, (err, data)=> {
+        if(err){
+            console.log("Erro ao processar a requisição.")
+            return res.status(500).json(data)
+        }
+        else{
+            console.log("Dados dos usuários obtidos com sucesso.")
+            return res.status(200).json(data)
+        }
+    })
+}
+
+export const addUsers = (req, res) => {
+    const sql = "insert into usuario (nome) value (?)"
+
+    const {nome} = req.body
+
+    db.query(sql, [nome], (err, data) => {
+        if(err){
+            console.log("Erro ao processar a requisição")
+            return res.status(500).json(err)
+        }
+        else{
+            console.log("Requisição bem sucedida!")
+            return res.status(201).json(data)
+        }
+    })
+}
+
+export const updateUsers = (req, res) => {
+    const sql = "update usuario set nome = ? where id = ?"
+
+    const {nome, id } = req.body;
+
+    db.query
+}
